478ea0068badb7ab2aa1423b0e0d4bfb2e4b1ea5
Daviology38 <mariofire38@gmail.com>
2021-08-31 12:20:07 -0400

    Updated formatting to PEP8

--- kmeans_ci2.py
+++ kmeans_ci2.py
@@ -41,8 +41,7 @@
 """
 
 
-
-#Imports
+# Imports
 import os
 import numpy as np
 from netCDF4 import Dataset
@@ -52,23 +51,23 @@ from sklearn.cluster import KMeans
 
 def kmeans_ci(X=None, stand=None, weighting=None, prop=None, nclus=None, nsim=None):
 
-    #Check if the array has been standardized prior to running kmeans_ci
+    # Check if the array has been standardized prior to running kmeans_ci
     if stand != None:
-        
-        #Standardize the grid point values by using the mean and standard deviation
+
+        # Standardize the grid point values by using the mean and standard deviation
         nan_mean = np.nanmean(X, axis=0)
         nan_std = np.nanstd(X, axis=0, ddof=1)
         X = (X - nan_mean) / nan_std
-    
+
     r = X.shape[0]
     c = X.shape[1]
     nclus = nclus
     nsim = nsim
 
-    #Check if EOF pre-filtering has been performed on the array
+    # Check if EOF pre-filtering has been performed on the array
     if prop != None:
-        
-        #Find the Singular Value Decomposition for the array X
+
+        # Find the Singular Value Decomposition for the array X
         U, S, V = np.linalg.svd(X)
         # NOTE in Python S is returned as diag(S) and V needs to be transposed (V.T)
         V = V.T
@@ -77,92 +76,96 @@ def kmeans_ci(X=None, stand=None, weighting=None, prop=None, nclus=None, nsim=No
         a = np.where(np.cumsum(sc, axis=1) > prop)
         a = a[0] + 1
         PC = U[:, 0:a] @ diag(S)[0:a, 0:a]
-        print('Pre-filtering using EOF retaining the first ' + str(a) + ' components done ...')
-        
+        print(
+            "Pre-filtering using EOF retaining the first "
+            + str(a)
+            + " components done ..."
+        )
+
     else:
-        
+
         PC = X
-        print('No EOF prefiltering ...')
-    
-    #Declare arrays to hold the cluster assignments and CI value
+        print("No EOF prefiltering ...")
+
+    # Declare arrays to hold the cluster assignments and CI value
     Kens = np.ones((PC.shape[0], nclus, nsim))
     K = np.zeros(PC.shape[0])
     CI = np.zeros(1)
-    
-    #Loop through the data array once (redundant here, but left in for consistency to MATLAB script)
+
+    # Loop through the data array once (redundant here, but left in for consistency to MATLAB script)
     for NC in range(0, 1, 1):
-        
-        #Set arraus to hold the data
+
+        # Set arraus to hold the data
         mean_cluster = np.zeros((nclus, c))
         ACCmax = np.zeros((nclus, nsim))
         k = np.zeros((PC.shape[0], nsim))
         MC = np.zeros(((nclus) * c, nsim))
 
-        print(['K means clustering with ' + str(nclus) + ' clusters begins ...'])
-        
-        #Loop through the number of simulations and perform kmeans
+        print(["K means clustering with " + str(nclus) + " clusters begins ..."])
+
+        # Loop through the number of simulations and perform kmeans
         for i in range(nsim):
-            
-            k[:, i] = KMeans(n_clusters=nclus, init='k-means++', 
-                             n_init=1, max_iter=1000
-                             ).fit_predict(X)  
-            
-            #Calculate the mean of each cluster using the assigned cluster values for 
-            #each row in array X
+
+            k[:, i] = KMeans(
+                n_clusters=nclus, init="k-means++", n_init=1, max_iter=1000
+            ).fit_predict(X)
+
+            # Calculate the mean of each cluster using the assigned cluster values for
+            # each row in array X
             for j in range(nclus):
-                
+
                 lj = len(np.nonzero(k[:, i] == j)[0])
-                
+
                 if lj > 1:
-                    
-                    mean_cluster[j, :] = np.mean(PC[np.nonzero(k[:, i] == j)[0], :],
-                                                 axis=0)
+
+                    mean_cluster[j, :] = np.mean(
+                        PC[np.nonzero(k[:, i] == j)[0], :], axis=0
+                    )
                 else:
-                    
+
                     mean_cluster[j, :] = X[k[:, i] == j]
-            
-            #Standardize the new array of means for the clusters        
+
+            # Standardize the new array of means for the clusters
             nan_mean = np.nanmean(mean_cluster.T, axis=0)
             nan_std = np.nanstd(mean_cluster.T, axis=0, ddof=1)
             mean_cluster2 = (mean_cluster.T - nan_mean) / nan_std
-            
-            #Reset the mean_cluster array to zeros and store the standardized mean_cluster2 array. 
-            #This array contains the centroids of each cluster.
-            #Note: We need the 'F' option to store it in Fortran style ordering
+
+            # Reset the mean_cluster array to zeros and store the standardized mean_cluster2 array.
+            # This array contains the centroids of each cluster.
+            # Note: We need the 'F' option to store it in Fortran style ordering
             mean_cluster = np.zeros((nclus, c))
-            MC[:, i] = mean_cluster2.flatten('F')  
-        
-        #Store all the cluster values over all simulations.    
+            MC[:, i] = mean_cluster2.flatten("F")
+
+        # Store all the cluster values over all simulations.
         Kens[:, NC - 1, :] = k
-        
-        #Calculate the Anomaly Correlation Coefficient (ACC) between all columns (centroids) 
-        #of the MC array. This allows the user to determine the optimal arrangement
-        #of centroids that maximizes the ACC.
+
+        # Calculate the Anomaly Correlation Coefficient (ACC) between all columns (centroids)
+        # of the MC array. This allows the user to determine the optimal arrangement
+        # of centroids that maximizes the ACC.
         for i in range(nclus):
-            
+
             for j in range(nsim):
-                
-                sample1 = MC[(i*c):(i+1) * c, j]
+
+                sample1 = MC[(i * c) : (i + 1) * c, j]
                 a = np.nonzero(j != np.arange(0, nsim))[0]
-                sample2 = MC[:, a].reshape(c, (nsim-1) * nclus,
-                                           order='F').copy()
+                sample2 = MC[:, a].reshape(c, (nsim - 1) * nclus, order="F").copy()
                 ind = np.isnan(sample1)
                 sample1[ind] = 0
                 ind = np.isnan(sample2)
                 sample2[ind] = 0
-                ACC = (1 / (c-1)) * sample1.conj().T @ sample2
-                ACC = ACC.reshape(nclus, nsim-1, order='F').copy()
+                ACC = (1 / (c - 1)) * sample1.conj().T @ sample2
+                ACC = ACC.reshape(nclus, nsim - 1, order="F").copy()
                 ACCmax[i, j] = np.mean(ACC.max(0))
-        
-        #Find the optimal partition from the generated list of ACC values.
-        part = np.nonzero(np.mean(ACCmax, axis=0) 
-                          == np.max(np.mean(ACCmax, axis=0)))[0]
+
+        # Find the optimal partition from the generated list of ACC values.
+        part = np.nonzero(np.mean(ACCmax, axis=0) == np.max(np.mean(ACCmax, axis=0)))[0]
 
         # Classification index for the partition ton 'nclus' clusters
-        CI[NC] = np.mean(ACCmax)  
-        if len(part) > 1: part = part[0]
+        CI[NC] = np.mean(ACCmax)
+        if len(part) > 1:
+            part = part[0]
 
         # best partition that maximize the ACC with the nsim-1 other partitions
-        K[:] = np.squeeze(k[:, part])  
-        
+        K[:] = np.squeeze(k[:, part])
+
     return K, CI
